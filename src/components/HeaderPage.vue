ba79cde873ab3c31735e6540ea83ea761a409e39
AdrianBottini <adrianboh@gmail.com>
2022-11-24 16:53:28 +0100

    mod

---
+++ src/components/HeaderPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app" class="header">
+    <img alt="Vue logo" src="img/dc-logo.png">
+    <nav>
+      <ul>
+        <li>CARACTERS</li>
+        <li>COMICS</li>
+        <li>MOVIES</li>
+        <li>TV</li>
+        <li>GAMES</li>
+        <li>COLLECTIBILES</li>
+        <li>VIDEOS</li>
+        <li>FANS</li>
+        <li>NEWS</li>
+        <li>SHOP</li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderPage',
+};
+</script>
+
+<style>
+
+</style>
